91f374d9c302edfeb7ee2c23b28e17db597b1907
sunny tsai <409422491@mail.fju.edu.tw>
2020-12-29 01:02:38 +0800

    init3

--- untitled9.py
+++ untitled9.py
@@ -1,8 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Dec 26 16:43:54 2020
+products=[]
 
-@author: sunny
-"""
-import sys
-sys.path
+while True:
+    name=input("請輸入商品名稱:")
+
+    if name=='q':
+        break
+    price=input('請輸入商品價格:')
+    p=[name,price] #小清單 
+    products.append(p)
+    
+    
+print(products)
+
+for product in products:
+    print(product[0],'的價格是',product[1])
+
+with open('products.txt','w') as f:
+    for product in products:
+        f.write( p[0] + ',' +p[1])+ '\n' )
+
+
